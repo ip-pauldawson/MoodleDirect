84f3abc012f53584b0bbd0b3d0789a7750627cde
Paul Dawson <pdawson@iparadigms.com>
2012-02-21 18:23:46 +0000

    Added: $module->requires to version.php

--- turnitintool/version.php
+++ turnitintool/version.php
@@ -6,5 +6,6 @@
 
 $module->version  = 2012020201;  // The current module version (Date: YYYYMMDDXX)
 $module->cron     = 1800;        // Period for cron to check this module (secs)
+$module->requires = 2007101509;  // Requires version 1.9 or higher
 
 /* ?> */
