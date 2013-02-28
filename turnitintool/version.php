37eca1dc789444f3c56d53eefce853acf34a36ae
Paul Dawson <pdawson@iparadigms.com>
2013-02-28 15:49:33 +0000

    commenting Moodle.org plugin check requirements

--- turnitintool/version.php
+++ turnitintool/version.php
@@ -6,6 +6,7 @@
 
 $module->version  = 2012120403;  // The current module version (Date: YYYYMMDDXX)
 $module->cron     = 1800;        // Period for cron to check this module (secs)
-$module->requires = 2007101509;  // 1.9+
+//$module->requires = 2007101509;  // 1.9+
+//$module->requires = 2010112400;  // 2.0+
 
 /* ?> */
