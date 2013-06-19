afb36c8a9f0816c27a303ebbcf89e48e928e3c5b
Paul Dawson <pdawson@iparadigms.com>
2013-06-19 12:25:27 +0100

    Removing the Moodle 2.0+ check, still supporting 1.9+ in one build

--- turnitintool/version.php
+++ turnitintool/version.php
@@ -7,6 +7,6 @@
 $module->version  = 2012120408;  // The current module version (Date: YYYYMMDDXX)
 $module->cron     = 1800;        // Period for cron to check this module (secs)
 //$module->requires = 2007101509;  // 1.9+
-$module->requires = 2010112400;  // 2.0+
+//$module->requires = 2010112400;  // 2.0+
 
 /* ?> */
