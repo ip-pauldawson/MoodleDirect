561fb828af743cc7d4347f05fed08fba3c161e74
John McGettrick <jmcgettrick@iparadigms.com>
2015-08-14 08:13:49 +0100

    Remove different function for edge cases with case sensitivity.

--- db/upgrade.php
+++ db/upgrade.php
@@ -479,11 +479,7 @@ function xmldb_turnitintool_upgrade($oldversion) {
     if ($result && $oldversion < 2015030303) {
         // Update URL for UK accounts.
         $apiurl = get_config('', 'turnitin_apiurl');
-        if (is_callable('str_ireplace')) {
-            $newurl = str_ireplace('submit.ac.uk', 'api.turnitinuk.com', $apiurl);
-        } else {
-            $newurl = str_replace('submit.ac.uk', 'api.turnitinuk.com', $apiurl);
-        }
+        $newurl = str_replace('submit.ac.uk', 'api.turnitinuk.com', strtolower($apiurl));
         set_config('turnitin_apiurl', $newurl);
     }
 
